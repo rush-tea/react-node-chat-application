221549d87b8d89140ed55ec01578654cd79887bb
rush-tea <tripathiadarsh39@gmail.com>
2021-04-05 16:11:33 +0530

    changes

--- client/src/components/Chat/Chat.js
+++ client/src/components/Chat/Chat.js
@@ -66,8 +66,8 @@ const Chat = ({location}) => {
                 <InfoBar room={room}/>
                 <Messages messages={messages} name={name} />
                 <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
-                <TextContainer users={users} />
             </div>
+            <TextContainer users={users} />
         </div>
     )
 }
