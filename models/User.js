0f6c8cb0dc1a97a216c767b4526d2bb7727be855
rush-tea <tripathiadarsh39@gmail.com>
2021-04-18 00:34:05 +0530

    added basic routes and models

---
+++ models/User.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide a email"],
+        unique: true,
+        match: [
+            /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/,
+            "Please provide a valid email"
+        ]
+    },
+    password: {
+        type: String,
+        required: [true, "Please add a password"],
+        minlength: 6,
+        select: false
+    },
+    resetPasswordToken: String,
+    reserPasswordExpire: Date
+});
+
+const User = mongoose.model("User",UserSchema);
+
+module.exports = User;
